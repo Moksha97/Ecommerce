2ba8debd88a916d0a4e941805e4f3db091e52b74
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-03-10 16:53:14 -0600

    added proper mysql conn

--- backend/database/conn.js
+++ backend/database/conn.js
@@ -1,23 +1,21 @@
-
-const mysql = require('mysql2');
+const mysql = require("mysql2/promise");
 
 // Exports a connection to the database
-const conn = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database:  process.env.MYSQL_DATABASE
-  });
-  
-conn.connect(function(err) {
-if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-}
-
-console.log('connected as id ' + conn.threadId);
+const conn = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
-  
-module.exports = conn
 
+conn
+  .getConnection()
+  .then((connection) => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database: " + err);
+  });
+
+module.exports = conn;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var pool = require('../database/conn')
+var ash = require("express-async-handler")
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  return res.json({ message: 'Welcome to the API' });
-});
+// query db
+router.get("/", ash(async function (req, res, next) {
+  const [result] = await pool.query('select ?+? as sum', [2, 3]);
+  res.json(result);
+}));
 
 module.exports = router;
