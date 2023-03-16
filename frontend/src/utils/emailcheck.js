e1ebffdc4d77a0370bbd6593f7e18b44273983d4
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-03-16 11:58:23 -0500

    init

--- frontend/src/utils/emailcheck.js
+++ frontend/src/utils/emailcheck.js
@@ -3,9 +3,3 @@ export const emailcheck = (email) => {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 };
-
-// Path: frontend/src/utils/passwordcheck.js
-export const passwordcheck = (password) => {
-  const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-  return re.test(password);
-};
