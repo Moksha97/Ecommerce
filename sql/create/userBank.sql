6eec4a842ae9c6fa5596bd7332ac24bcf5b5d6c0
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-02 12:16:52 -0500

    Update userBank.sql

--- sql/create/userBank.sql
+++ sql/create/userBank.sql
@@ -1,7 +1,7 @@
 CREATE TABLE userbank
 (
     accountnumber INT,
-    username      VARCHAR(50),
+    username      VARCHAR(40),
     primary key userbankid (accountnumber, username),
     FOREIGN KEY (accountnumber) REFERENCES bankaccount (accountnumber),
     FOREIGN KEY (username) REFERENCES user (username)
