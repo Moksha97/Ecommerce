07fbdb92b637833b5bd680be8fbd9f920ce44fc0
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 22:22:50 -0500

    Update productRating.sql

--- sql/create/productRating.sql
+++ sql/create/productRating.sql
@@ -2,7 +2,7 @@ CREATE TABLE productrating
 (
     pid      INT,
     username VARCHAR(40),
-    rating   DECIMAL(3, 2),
+    rating   DECIMAL(3, 2) NOT NULL,
     PRIMARY KEY prid (pid, username),
     FOREIGN KEY (pid) REFERENCES product (pid),
     FOREIGN KEY (username) REFERENCES user (username)
