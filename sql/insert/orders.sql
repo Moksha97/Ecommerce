dc41c086de2e9b9c01f6426ddf2d45e3218f1ab9
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 21:04:58 -0500

    Update orders.sql

--- sql/insert/orders.sql
+++ sql/insert/orders.sql
@@ -1,4 +1,4 @@
 INSERT INTO `Order` (oid, status, timestamp, username, aid, payid)
-VALUES (1, 'Processing', '2022-12-25 10:30:00', 'john_doe@gmail.com', 101, 1),
-       (2, 'Shipped', '2022-12-26 11:45:00', 'jane_doe@gmail.com', 201, 2),
-       (3, 'Delivered', '2022-12-27 12:15:00', 'jack_smith@gmail.com', 301, 3);
+VALUES (1, 'PLACED', '2022-12-25 10:30:00', 'john_doe@gmail.com', 101, 1),
+       (2, 'SHIPPED', '2022-12-26 11:45:00', 'jane_doe@gmail.com', 201, 2),
+       (3, 'DELIVERED', '2022-12-27 12:15:00', 'jack_smith@gmail.com', 301, 3);
