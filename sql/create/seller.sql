2d8b2ec63d920408a99603a28504b924a9adcc0a
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 21:07:31 -0500

    Update seller.sql

--- sql/create/seller.sql
+++ sql/create/seller.sql
@@ -1,6 +1,6 @@
 CREATE TABLE seller
 (
-    sid           INT PRIMARY KEY,
+    sid           INT PRIMARY KEY AUTO_INCREMENT,
     sname         VARCHAR(20),
     accountnumber INT UNIQUE,
     FOREIGN KEY (accountnumber) REFERENCES bankaccount (accountnumber)
