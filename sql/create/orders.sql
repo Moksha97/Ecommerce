f7d9015370e5a941a6494cbbaf14c842036640e1
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 22:21:52 -0500

    Update orders.sql

--- sql/create/orders.sql
+++ sql/create/orders.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `order`
 (
     oid       INT PRIMARY KEY AUTO_INCREMENT,
-    status    ENUM("PLACED", "PACKED", "INTRANSIT", "DELIVERED", "CANCELED"),
-    timestamp DATETIME,
-    username  varchar(40),
-    aid       INT,
-    payid     INT UNIQUE,
+    status    ENUM("PLACED", "PACKED", "INTRANSIT", "DELIVERED", "CANCELED") NOT NULL,
+    timestamp DATETIME NOT NULL,
+    username  varchar(40) NOT NULL,
+    aid       INT NOT NULL,
+    payid     INT UNIQUE NOT NULL,
     FOREIGN KEY (username) REFERENCES user (username),
     FOREIGN KEY (aid) REFERENCES address (aid),
     FOREIGN KEY (payid) REFERENCES payment (payid)
