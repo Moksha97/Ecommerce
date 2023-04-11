04b8103fd2e85cf2f91f9e79fa1dc3daa662ce9a
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 20:58:32 -0500

    Update product.sql

--- sql/insert/product.sql
+++ sql/insert/product.sql
@@ -1,5 +1,4 @@
 INSERT INTO product (pid, pname, pdesc, pcategory)
-VALUES (101, 'Samsung Galaxy S21', 'A high-end smartphone from Samsung', 'Electronics'),
-       (102, 'Apple MacBook Pro', 'A powerful laptop from Apple', 'Electronics'),
-       (103, 'Nike Air Zoom Pegasus 38', 'Running shoes from Nike', 'Sports');
-      
+VALUES (101, 'Samsung Galaxy S21', 'A high-end smartphone from Samsung', 'ELECTRONICS'),
+       (102, 'Apple MacBook Pro', 'A powerful laptop from Apple', 'ELECTRONICS'),
+       (103, 'Nike Air Zoom Pegasus 38', 'Running shoes from Nike', 'FASHION');
