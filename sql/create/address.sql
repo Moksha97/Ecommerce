602bd9bc81c4d9d1ca9ffd089481e77f323e12c1
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 22:19:04 -0500

    Update address.sql

--- sql/create/address.sql
+++ sql/create/address.sql
@@ -1,12 +1,12 @@
 CREATE TABLE address
 (
-    aid      INT NOT NULL AUTO_INCREMENT,
-    line1    VARCHAR(255),
+    aid      INT AUTO_INCREMENT,
+    line1    VARCHAR(255) NOT NULL,
     line2    VARCHAR(255),
-    city     VARCHAR(255),
-    state    VARCHAR(50),
-    zip      INT,
-    username VARCHAR(40),
+    city     VARCHAR(255) NOT NULL,
+    state    VARCHAR(50) NOT NULL,
+    zip      INT NOT NULL,
+    username VARCHAR(40) NOT NULL,
     PRIMARY KEY (aid),
     FOREIGN KEY (username) REFERENCES user (username)
 );
