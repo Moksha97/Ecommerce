860bd5af91cfbac59517a8093f6f1b749809e2b4
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 21:05:42 -0500

    Update bankAcocunt.sql

--- sql/create/bankAcocunt.sql
+++ sql/create/bankAcocunt.sql
@@ -1,6 +1,6 @@
 CREATE TABLE bankaccount
 (
-    accountnumber INT PRIMARY KEY,
+    accountnumber INT PRIMARY KEY AUTO_INCREMENT,
     branchcode    VARCHAR(10),
     bank          VARCHAR(50),
     routingnumber VARCHAR(20)
