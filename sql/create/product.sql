69661d64ae6593d887720fbf168e95a506b86134
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-10 21:07:11 -0500

    Update product.sql

--- sql/create/product.sql
+++ sql/create/product.sql
@@ -1,6 +1,6 @@
 CREATE TABLE product
 (
-    pid       INT PRIMARY KEY,
+    pid       INT PRIMARY KEY AUTO_INCREMENT,
     pname     VARCHAR(50),
     pdesc     VARCHAR(200),
     pcategory ENUM("FASHION", "HOME", "TOYS", "HEALTH", "ELECTRONICS")
