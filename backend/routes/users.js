00fb5d65cb8a68f84d2d7248c90e276abcb07623
poojaminna2203 <poojaminna2203@gmail.com>
2023-04-19 00:39:04 -0500

    get/Update User Details

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,49 @@
 var express = require("express");
 var router = express.Router();
+var db = require("../database/conn");
+var ash = require("express-async-handler");
+
+async function getDetails(username) {
+  const [rows] = await db.query(
+    "SELECT fname,lname,password,isadmin,preferredaddress,preferredaccount FROM user WHERE username = ?",
+    [username]
+  );
+  return rows;
+}
 
 /* GET users listing. */
 router.get("/", function (req, res) {
   return res.json({ message: "List of users" });
 });
+
+/* Get user details like fnmae, lnmae so on*/
+router.get(
+  "/getUser",
+  ash(async (req, res) => {
+    const username = req.username;
+    const rows = await getDetails(username);
+    res.json(rows);
+  })
+);
+
+async function updateDetails(username, fieldName, fieldValue) {
+  const [result] = await db.query(`UPDATE user SET ? = ? WHERE username = ?`, [
+    fieldName,
+    fieldValue,
+    username,
+  ]);
+  return result;
+}
+
+/* Update user details */
+router.put(
+  "/updateUser",
+  ash(async (req, res) => {
+    const username = req.username;
+    const { fieldName, fieldValue } = req.body;
+    await updateDetails(username, fieldName, fieldValue);
+    res.send("User details updated successfully");
+  })
+);
 
 module.exports = router;
