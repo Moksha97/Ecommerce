5eeace3a36b113d1c41222a4a6adcb63197fe14f
Badrinath Reddy <badrinathgopugari@gmail.com>
2023-04-21 11:04:53 -0500

    Update payment.sql

--- sql/create/payment.sql
+++ sql/create/payment.sql
@@ -2,6 +2,6 @@ CREATE TABLE payment
 (
     payid           INT PRIMARY KEY AUTO_INCREMENT,
     paymentstatus   ENUM("SUCCESS", "PENDING", "FAILED", "REFUND") NOT NULL,
-    userbankaccount INT NOT NULL,
-    FOREIGN KEY (userbankaccount) REFERENCES userbank (accountid)
+    accountid       INT NOT NULL,
+    FOREIGN KEY (accountid) REFERENCES userbank (accountid)
 );
