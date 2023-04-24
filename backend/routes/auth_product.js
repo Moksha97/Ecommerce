94a425b297c49d8013d223612a5b344eec499f0e
Badrinath Reddy Gopugari <badrinathgopugari@gmail.com>
2023-04-24 12:27:31 -0500

    user

--- backend/routes/auth_product.js
+++ backend/routes/auth_product.js
@@ -49,4 +49,30 @@ router.post(
   })
 );
 
+router.get(
+  "/rate/:pid",
+  ash(async (req, res) => {
+    const username = req.username;
+    const pid = req.params.pid;
+
+    // check if product is valid
+    if (pid === null || pid === undefined || pid === "") {
+      res.status(400).json({ error: "Product is not valid" });
+      return;
+    }
+
+    const [productRating] = await db.query(
+      "Select * from productrating where username = ? and pid = ?",
+      [username, pid]
+    );
+
+    if (productRating.length > 0) {
+      // console.log(productRating);
+      res.json({ rating: productRating[0].rating });
+    } else {
+      res.json({ rating: 0 });
+    }
+  })
+);
+
 module.exports = router;
